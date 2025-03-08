3d9bfd61258f375ead7e7188316d3df74e52d6e1
kallehahtela <sk1mb4yt@gmail.com>
2025-03-08 12:38:40 +0200

    Cleared these StreamChat functionalities and added providers.

--- src/app/(home)/channel/[cid].tsx
+++ src/app/(home)/channel/[cid].tsx
@@ -1,6 +1,7 @@
 import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
 import { ActivityIndicator, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Channel as ChannelType } from 'stream-chat';
 import { Channel, MessageInput, MessageList, useChatContext } from "stream-chat-expo";
 
@@ -26,7 +27,9 @@ export default function ChannelScreen() {
     return (
         <Channel channel={channel}>
             <MessageList />
-            <MessageInput />
+            <SafeAreaView edges={['bottom']}>
+                <MessageInput />
+            </SafeAreaView>
         </Channel>
     );
 }
---
+++ src/providers/ChatProvider.tsx
@@ -0,0 +1,43 @@
+import { StreamChat } from 'stream-chat';
+import { Chat, OverlayProvider} from 'stream-chat-expo';
+import { PropsWithChildren, useEffect, useState } from "react";
+import { ActivityIndicator } from 'react-native';
+
+const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
+
+export default function ChatProvider({children}: PropsWithChildren) {
+    const [isReady, setIsReady] = useState(false);
+
+    useEffect(() => {
+        const connect = async () => {
+            await client.connectUser(
+                {
+                  id: "jlahey",
+                  name: "Jim Lahey",
+                  image: "https://i.imgur.com/fR9Jz14.png",
+                },
+                client.devToken('jlahey'),
+              );
+              setIsReady(true);
+            };
+        
+        connect();
+
+        return () => {
+            client.disconnectUser();
+            setIsReady(false);
+        }
+    }, []);
+
+    if (!isReady) {
+        return <ActivityIndicator />;
+    }
+
+    return (
+        <OverlayProvider>
+            <Chat client={client}>
+                {children}
+            </Chat>
+        </OverlayProvider>
+    );
+}
