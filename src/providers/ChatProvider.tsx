c24df712bb06ec2bfffe54cb6a3dc2e0df95c76f
kallehahtela <sk1mb4yt@gmail.com>
2025-03-10 23:24:32 +0200

    Supabase edge function for server side stream token generation.

--- src/providers/ChatProvider.tsx
+++ src/providers/ChatProvider.tsx
@@ -4,10 +4,11 @@ import { PropsWithChildren, useEffect, useState } from "react";
 import { ActivityIndicator } from 'react-native';
 import { useAuth } from './AuthProvider';
 import { supabase } from '../lib/supabase';
+import { tokenProvider } from '../utils/tokenProvider';
 
 const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
 
-export default function ChatProvider({children}: PropsWithChildren) {
+export default function ChatProvider({ children }: PropsWithChildren) {
     const [isReady, setIsReady] = useState(false);
     const { profile } = useAuth();
 
@@ -17,6 +18,7 @@ export default function ChatProvider({children}: PropsWithChildren) {
         }
 
         const connect = async () => {
+
             await client.connectUser(
                 {
                   id: profile.id,
@@ -25,7 +27,7 @@ export default function ChatProvider({children}: PropsWithChildren) {
                   .from('avatars')
                   .getPublicUrl(profile.avatar_url).data.publicUrl,
                 },
-                client.devToken(profile.id),
+                tokenProvider
               );
               setIsReady(true);
             };
