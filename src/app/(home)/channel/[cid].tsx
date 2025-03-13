893c9c31500fe31c730fbeeaba1d62e4bb702c76
kallehahtela <sk1mb4yt@gmail.com>
2025-03-13 15:01:31 +0200

    Eas build for Iphone real device by UDID.

--- src/app/(home)/channel/[cid].tsx
+++ src/app/(home)/channel/[cid].tsx
@@ -25,7 +25,7 @@ export default function ChannelScreen() {
     }
 
     return (
-        <Channel channel={channel}>
+        <Channel channel={channel} audioRecordingEnabled={true}>
             <MessageList />
             <SafeAreaView edges={['bottom']}>
                 <MessageInput />
